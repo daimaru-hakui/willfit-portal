8552fc2ffc8ba5e6f974f7986454a75a79f1164b
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-12-01 10:46:21 +0900

    alchrol check

--- app/dashboard/alcohol-check/page.tsx
+++ app/dashboard/alcohol-check/page.tsx
@@ -3,7 +3,7 @@ import AlcoholCheckTable from "./components/alcohol-check-table";
 import { Container, Paper } from "@mantine/core";
 const AlcoholCheckPage = () => {
   return (
-    <Container maw={800}>
+    <Container maw={400}>
       <Paper shadow="xs" p="xl" bg="white">
         <AlcoholCheckTable />
       </Paper>
