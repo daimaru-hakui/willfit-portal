670f54234bbe296433e4b3feb7ed701809624800
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-12-05 15:36:52 +0900

    daimaru user

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -1,12 +1,29 @@
 "use client";
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Link from "next/link";
 import { Flex, Text, Box, Button } from "@mantine/core";
 import HeaderMenu from "./header-menu";
 import HeaderDrawer from "./header-drawer";
 import { navList } from "@/utils/nav-list";
+import { useSession } from "next-auth/react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/lib/firebase/client";
 
 const NavBar: FC = () => {
+  const session = useSession();
+  const uid = session.data?.user.uid;
+  const [isDaimaru, setIsDaimaru] = useState(false);
+  
+  useEffect(() => {
+    const getDaimaruUser = async () => {
+      const docRef = doc(db, "authority", `${uid}`);
+      const snapShot = await getDoc(docRef);
+      const bool = snapShot.data()?.daimaru;
+      setIsDaimaru(bool);
+    };
+    getDaimaruUser()
+  }, [uid]);
+
   return (
     <Flex w="100%" h="50px" justify="center">
       <Flex
@@ -24,6 +41,20 @@ const NavBar: FC = () => {
         </Link>
         <Flex align="center" gap={12}>
           <Flex gap={12} display={{ base: "none", sm: "flex" }}>
+            {isDaimaru && (
+              <Link
+                href={`https://daimaru-portal.vercel.app/`}
+                rel="noopener noreferrer"
+              >
+                <Button
+                  variant=""
+                  size="xs"
+                  className="hidden lg:inline-block px-2"
+                >
+                  <span>大丸白衣</span>
+                </Button>
+              </Link>
+            )}
             {navList.map(({ title, path, target }, index) => (
               <Box key={index}>
                 <Link
